3e54e1de22beb61374dd5d05194528a55ce89d19
João Vitor <joaoaansaloni@gmail.com>
2024-07-29 22:17:37 -0300

    Restante da lógica

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
- A idea desse projeto é criar um contador simples utilizando Html, Css e JavaScript. O contador vai ter as funcionalidades de aumentar, decrescer e resetar e utilizando estruturas condicionais para alterar texto, cor de acordo com a mudança dos elementos. 
+ A idea desse projeto é criar um contador simples utilizando Html, Css e JavaScript. O contador vai ter as funcionalidades de aumentar, decrescer e resetar utilizando evento on-click.
--- app.js
+++ app.js
@@ -3,19 +3,30 @@ let contador = 0;
 function exibirNumero(contador){
     let numero = document.querySelector('span');
     numero.innerHTML = contador;
+    if (contador > 0) {
+       let botao =  document.getElementById('value')
+       botao.style.color = "green";
+    }else if(contador < 0){
+       let botao =  document.getElementById('value')
+       botao.style.color = "red";
+    }else if(contador == 0){
+        let botao =  document.getElementById('value')
+       botao.style.color = "black";
+    }
 }
 
 function increase(){
-    let valorAumentado = contador ++;
-    exibirNumero(valorAumentado);  
+     contador = contador+1;
+     exibirNumero(contador);
 }
 
 function decrease()
 {
-    let valorDiminuido = contador --;
-    exibirNumero(valorDiminuido);
+    contador = contador -1;
+    exibirNumero(contador);
 }
 
 function reset(){
-    exibirNumero(0);
-}
+    contador = 0;
+    exibirNumero(contador); 
+}   
